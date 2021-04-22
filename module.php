5a28477d07aa88d9996eafd13d71a188d51b226f
Hifi58 <gillet.tony@gmail.com>
2021-04-22 16:47:50 +0200

    ajout html

---
+++ module.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="multiplication.php" method="get" class="form-example">
+    <input type="text" title="nombre 1" value="Nombre 1">
+    <p>x</p>
+    <input type="text" title="nombre 2" value="Nombre2">
+    <p>=</p>
+    <button>Résultat</button>
+    </form>
+
+</body>
+</html>
