9bd2be1e92578faf378ae7b44e928dbb991c04cc
Hifi58 <gillet.tony@gmail.com>
2021-04-22 16:37:29 +0200

    MAJ

--- multiplication.php
+++ multiplication.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
 <?php 
  $x = 3;
  $y = 0;
@@ -15,8 +5,5 @@
  while ($y <= 10){
      echo '<p>' .$x. 'x' .$y. '=' .$x*$y++.'</p>'; 
  }
-?>
 
- </body>
-</html>
 
