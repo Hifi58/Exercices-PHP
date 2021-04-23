1417f2efef8047cea72cff9ddc2c32dca704a704
Hifi58 <gillet.tony@gmail.com>
2021-04-23 13:32:07 +0200

    V3

--- module.php
+++ module.php
@@ -22,21 +22,6 @@
     <option value="9">9</option>
     <option value="10">10</option>
 
-<!-- </select>
-    <p>x</p>
-    <select name="number2">
-    <option value="choix1">1</option>
-    <option value="choix2">2</option>
-    <option value="choix3">3</option>
-    <option value="choix4">4</option>
-    <option value="choix1">5</option>
-    <option value="choix2">6</option>
-    <option value="choix3">7</option>
-    <option value="choix4">8</option>
-    <option value="choix1">9</option>
-    <option value="choix2">10</option>
-
-</select> -->
     <p>=</p>
     <input type="submit" value="Résultat" />
 </form>
---
+++ module2.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="multiplication3.php" method="post">
+  <fieldset>
+  <legend>Veuillez sélectionner vos tables</legend>
+  <div>
+      <input type="checkbox" name="table" value="0">
+      <label for="0">0</label>
+    </div>
+    <div>
+      <input type="checkbox" name="table" value="1">
+      <label for="1">1</label>
+    </div>
+    <div>
+      <input type="checkbox" name="table" value="2">
+      <label for="2">2</label>
+    </div>
+    <div>
+      <input type="checkbox" name="table" value="3">
+      <label for="3">3</label>
+    </div>
+    <div>
+      <input type="checkbox" name="table" value="4">
+      <label for="4">4</label>
+    </div>
+    <div>
+      <input type="checkbox" name="table" value="5">
+      <label for="5">5</label>
+    </div>
+    <div>
+      <input type="checkbox" name="table" value="6">
+      <label for="6">6</label>
+    </div>
+    <div>
+      <input type="checkbox" name="table" value="7">
+      <label for="7">7</label>
+    </div>
+    <div>
+      <input type="checkbox" name="table" value="8">
+      <label for="8">8</label>
+    </div>
+    <div>
+      <input type="checkbox" name="table" value="9">
+      <label for="9">9</label>
+    </div>
+    <div>
+      <input type="checkbox" name="table" value="10">
+      <label for="10">10</label>
+    </div>
+    <div>
+      <button type="submit">Envoyer le formulaire</button>
+    </div>
+  </fieldset>
+</form>
+</body>
+</html>
